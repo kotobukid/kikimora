3f6a64f93f2df86cf8d6186eaa5632adb47b7307
kotobukid <kotobuki@daqua.jp>
2021-07-04 15:04:30 +0900

    掃除コマンド実行時に発言者の作成していないチャンネルもチェック対象とするようにした

--- orders/wipe.js
+++ orders/wipe.js
@@ -5,29 +5,42 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var models_1 = require("../models");
 var lodash_1 = __importDefault(require("lodash"));
+var async_1 = __importDefault(require("async"));
 var func = function (client, msg) {
     models_1.find_channel({
-        owner: msg.author.id,
         is_deleted: false
     }).then(function (channels) {
-        var _loop_1 = function (i) {
-            var ch = channels[i];
-            client.channels.fetch(channels[i].text_channel, false, true).then(function (tc) {
-            }).catch(function (e) {
-                models_1.find_channel({
-                    id: ch.id
-                }).then(function (channels_to_delete) {
-                    lodash_1.default.each(channels_to_delete, function (ch_d) {
-                        // @ts-ignore
-                        ch_d.update({ is_deleted: true }).then();
-                        console.log("\u524A\u9664\u6E08\u307F\u30C1\u30E3\u30F3\u30CD\u30EB:" + ch_d.channel_name + " \u306E\u72B6\u614B\u3092\u30C7\u30FC\u30BF\u30D9\u30FC\u30B9\u306B\u53CD\u6620\u3055\u305B\u307E\u3057\u305F\u3002");
+        var funcs = lodash_1.default.map(channels, function (ch) {
+            return function (done) {
+                client.channels.fetch(ch.text_channel, false, true).then(function (tc) {
+                    done(null, null);
+                }).catch(function (e) {
+                    models_1.find_channel({
+                        id: ch.id
+                    }).then(function (channels_to_delete) {
+                        if (channels_to_delete.length > 0) {
+                            // @ts-ignore
+                            channels_to_delete[0].update({ is_deleted: true }).then(function () {
+                                done(null, '#' + channels_to_delete[0].channel_name);
+                            });
+                        }
+                        else {
+                            done(e, null);
+                        }
                     });
                 });
-            });
-        };
-        for (var i = 0; i < channels.length; i++) {
-            _loop_1(i);
-        }
+            };
+        });
+        // @ts-ignore
+        async_1.default.series(funcs, function (e, results) {
+            var deleted = lodash_1.default.filter(results, function (r) { return r; });
+            if (deleted.length > 0) {
+                msg.channel.send("<@!" + msg.author.id + ">\u524A\u9664\u6E08\u307F\u30C1\u30E3\u30F3\u30CD\u30EB\u306E\u72B6\u614B\u3092\u30C7\u30FC\u30BF\u30D9\u30FC\u30B9\u306B\u53CD\u6620\u3055\u305B\u307E\u3057\u305F\u3002\n" + deleted.join('\n'));
+            }
+            else {
+                msg.channel.send("<@!" + msg.author.id + ">\u51E6\u7F6E\u306E\u5FC5\u8981\u306F\u3042\u308A\u307E\u305B\u3093\u3067\u3057\u305F\u3002");
+            }
+        });
     }).catch(function (err) {
         console.error(err);
     });
--- orders/wipe.ts
+++ orders/wipe.ts
@@ -1,30 +1,43 @@
 import {KikimoraClient} from "../types";
 import {find_channel} from "../models";
-import {Channel, ChannelSource} from "../models/channel";
+import {Channel} from "../models/channel";
 import _ from 'lodash';
+import async, {AsyncFunction} from 'async';
 
 const func = (client: KikimoraClient, msg: any) => {
     find_channel({
-        owner: msg.author.id,
         is_deleted: false
     }).then((channels: Channel []) => {
-        for (let i: number = 0; i < channels.length; i++) {
-            const ch: Channel = channels[i];
-
-            client.channels.fetch(channels[i].text_channel, false, true).then(tc => {
-            }).catch((e: Error) => {
-                find_channel({
-                    id: ch.id
-                }).then((channels_to_delete: Channel[]) => {
-                    _.each(channels_to_delete, (ch_d: ChannelSource) => {
-                        // @ts-ignore
-                        ch_d.update({is_deleted: true}).then();
-
-                        console.log(`削除済みチャンネル:${ch_d.channel_name} の状態をデータベースに反映させました。`);
+        const funcs: AsyncFunction<unknown, Error>[] = _.map(channels, (ch: Channel) => {
+            return (done: Function) => {
+                client.channels.fetch(ch.text_channel, false, true).then(tc => {
+                    done(null, null);
+                }).catch((e: Error) => {
+                    find_channel({
+                        id: ch.id
+                    }).then((channels_to_delete: Channel[]) => {
+                        if (channels_to_delete.length > 0) {
+                            // @ts-ignore
+                            channels_to_delete[0].update({is_deleted: true}).then(() => {
+                                done(null, '#' + channels_to_delete[0].channel_name);
+                            })
+                        } else {
+                            done(e, null);
+                        }
                     })
-                })
-            });
-        }
+                });
+            };
+        });
+
+        // @ts-ignore
+        async.series(funcs, (e: any, results: any[]): void => {
+            const deleted: string[] = _.filter(results, r => r) as string[]
+            if (deleted.length > 0) {
+                msg.channel.send(`<@!${msg.author.id}>削除済みチャンネルの状態をデータベースに反映させました。\n${deleted.join('\n')}`)
+            } else {
+                msg.channel.send(`<@!${msg.author.id}>処置の必要はありませんでした。`);
+            }
+        });
     }).catch((err: Error) => {
         console.error(err)
     });
