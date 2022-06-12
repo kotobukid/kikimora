588111f9204246b75b4e11b3fdb86efce7d9dcc4
kotobukid <kotobuki@daqua.jp>
2022-06-12 23:44:43 +0900

    最終的なテキストも出力

--- sample_scripts/test_parse_datetime.js
+++ sample_scripts/test_parse_datetime.js
@@ -12,34 +12,52 @@ var assert_equal = function (a, b) {
         console.error("\n".concat(a, ", ").concat(b));
     }
 };
+var to_channel_name = function (r) {
+    if (r.m !== '') {
+        if (r.d !== '') {
+            console.log("".concat(r.m, "\u6708").concat(r.d, "\u65E5 ").concat(r.message_payload));
+        }
+        else {
+            console.log("".concat(r.m, "\u6708 ").concat(r.message_payload));
+        }
+    }
+    else {
+        console.log(r.message_payload);
+    }
+};
 (function () {
     var r = (0, parse_datetime_1.parse_datetime)('1225クリスマス');
     assert_equal(r.m, '12');
     assert_equal(r.d, '25');
     assert_equal(r.message_payload, 'クリスマス');
+    to_channel_name(r);
 })();
 (function () {
     var r = (0, parse_datetime_1.parse_datetime)('１２２５クリスマス');
     assert_equal(r.m, '12');
     assert_equal(r.d, '25');
     assert_equal(r.message_payload, 'クリスマス');
+    to_channel_name(r);
 })();
 (function () {
     var r = (0, parse_datetime_1.parse_datetime)('01 元旦');
     assert_equal(r.m, '01');
     assert_equal(r.d, '');
     assert_equal(r.message_payload, '元旦');
+    to_channel_name(r);
 })();
 (function () {
     var r = (0, parse_datetime_1.parse_datetime)('元旦');
     assert_equal(r.m, '');
     assert_equal(r.d, '');
     assert_equal(r.message_payload, '元旦');
+    to_channel_name(r);
 })();
 (function () {
-    var r = (0, parse_datetime_1.parse_datetime)('1元旦');
+    var r = (0, parse_datetime_1.parse_datetime)('8盆');
     assert_equal(r.m, '');
     assert_equal(r.d, '');
-    assert_equal(r.message_payload, '1元旦');
+    assert_equal(r.message_payload, '8盆');
+    to_channel_name(r);
 })();
 out('\n');
--- sample_scripts/test_parse_datetime.ts
+++ sample_scripts/test_parse_datetime.ts
@@ -13,11 +13,30 @@ const assert_equal = <T>(a: T, b: T): void => {
     }
 }
 
+declare type ParsedMessage = {
+    m: string,
+    d: string,
+    message_payload: string
+}
+
+const to_channel_name = (r: ParsedMessage) => {
+    if (r.m !== '') {
+        if (r.d !== '') {
+            console.log(`${r.m}月${r.d}日 ${r.message_payload}`);
+        } else {
+            console.log(`${r.m}月 ${r.message_payload}`);
+        }
+    } else {
+        console.log(r.message_payload);
+    }
+}
+
 (() => {
     const r = parse_datetime('1225クリスマス');
     assert_equal(r.m, '12');
     assert_equal(r.d, '25');
     assert_equal(r.message_payload, 'クリスマス');
+    to_channel_name(r);
 })();
 
 (() => {
@@ -25,6 +44,7 @@ const assert_equal = <T>(a: T, b: T): void => {
     assert_equal(r.m, '12');
     assert_equal(r.d, '25');
     assert_equal(r.message_payload, 'クリスマス');
+    to_channel_name(r);
 })();
 
 (() => {
@@ -32,6 +52,7 @@ const assert_equal = <T>(a: T, b: T): void => {
     assert_equal(r.m, '01');
     assert_equal(r.d, '');
     assert_equal(r.message_payload, '元旦');
+    to_channel_name(r);
 })();
 
 (() => {
@@ -39,13 +60,15 @@ const assert_equal = <T>(a: T, b: T): void => {
     assert_equal(r.m, '');
     assert_equal(r.d, '');
     assert_equal(r.message_payload, '元旦');
+    to_channel_name(r);
 })();
 
 (() => {
-    const r = parse_datetime('1元旦');
+    const r = parse_datetime('8盆');
     assert_equal(r.m, '');
     assert_equal(r.d, '');
-    assert_equal(r.message_payload, '1元旦');
+    assert_equal(r.message_payload, '8盆');
+    to_channel_name(r);
 })();
 
 out('\n');
